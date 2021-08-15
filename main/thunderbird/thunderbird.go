79735244354b937d6826a196f2cf894acd02e423
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2021-08-15 04:06:41 +0300

    Working on thunderbird backup struct.

--- main/thunderbird/thunderbird.go
+++ main/thunderbird/thunderbird.go
@@ -2,6 +2,12 @@ package thunderbird
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+const (
+	MsgFiltersFileName = "msgFilterRules.dat"
 )
 
 type Thunderbird struct {
@@ -16,4 +22,19 @@ func New(folderLoc string, backupLoc string) Thunderbird {
 
 func (thunderbird Thunderbird) Backup() {
 	fmt.Printf("Starting to backup filters from %s to %s \n", thunderbird.folderLoc, thunderbird.backupLoc)
+
+	files, err := ioutil.ReadDir(thunderbird.folderLoc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var filterFound = false
+	for _, f := range files {
+		if f.Name() == MsgFiltersFileName {
+			filterFound = true
+		}
+	}
+	if !filterFound {
+		log.Fatal("No file named \"%s\" found in given thunderbird filter folder", MsgFiltersFileName)
+	}
 }
